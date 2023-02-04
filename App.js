dbd5809d5e8b8dabc4933f425af647651b705c24
jnounna <97972572+jnounna@users.noreply.github.com>
2023-02-04 13:13:08 -0700

    uploading project

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import React, {useEffect, useState} from 'react';
+
 
 export default function App() {
+
+  
+
+  const [amount, setAmount] = useState(0);
+
+  const [spent, setSpent] = useState(0);
+
+  const [total, setTotal] = useState(0);
+  useEffect(() => 
+
+    { setTotal(() => amount - spent);
+    },[total]);
+
+ 
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Enter how much you have to start with this week:</Text>
+
+      <TextInput
+       style={styles.input} 
+       placeholder = 'e.g 90' 
+       onChangeText={(val) => setAmount(val)} />
+
+
+      <Text>You get to start the week with: {amount} </Text>
+
+      <Text>Enter how much you spent:</Text>
+
+      <TextInput
+       style = {styles.input}
+       placeholder = 'e.g 10'
+       onChangeText={(val2) => setSpent(val2)} />
+
+      <View style={styles.buttonContainer}>
+        <Button title='update amount' onPress={() => setTotal() }/>
+      </View>
+
+      <Text>New amount: {total}  </Text>
+      
     </View>
   );
 }
@@ -17,4 +56,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer:{
+    marginTop: 10,
+    marginBottom: 20
+  },
+  input:{
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 100,
+
+  }
 });
---
+++ README.md
@@ -0,0 +1,25 @@
+# Overview
+
+This is a basic project for me to start to learn how to program with reactnative using node js and javascript.
+The project is a basic allowance app.
+
+This project is a basic project where you input two numbers. The first number is the number that you have for a budget
+The second number that you put in is how much you spend to subtract from the allowance.
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Development Environment
+
+Node js and, expo, and react js.
+
+# Useful Websites
+
+{Make a list of websites that you found helpful in this project}
+* [Web Site Name](http://url.link.goes.here)
+* [Web Site Name](http://url.link.goes.here)
+
+# Future Work
+
+Some things that I would like to fix in the future. 
+* Make it look better.
+* Have it keep subtracting from the new value
